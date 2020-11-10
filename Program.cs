4e19c9802de02df9f9f05a59a7114785d02aa5b1
Diego <d_diazmendoza@hotmail.com>
2020-11-10 14:14:24 -0600

    third commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AlienLifeSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public enum StarType 
+    {
+        one, two, three
+    }
+    public class Star 
+    {
+        public float mass { get; set; }
+        public StarType type {get; set;}
+
+    }
+    
+}
