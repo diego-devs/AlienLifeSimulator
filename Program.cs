9cb233744d35bc4abea6a4630f95e55b23883b69
Diego <d_diazmendoza@hotmail.com>
2020-11-17 13:51:07 -0600

    20%

--- Program.cs
+++ Program.cs
@@ -1,37 +1,86 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AlienLifeSimulator
 {
+    public static class CosmicNames 
+    {
+        public static List<string> PlanetsNames = new List<string>() {"KEPLER", "SAGAN"};
+        public static List<string> StarsNames = new List<string>();
+    }
     public class Program
     {
         static void Main(string[] args)
         {
-            
+            Galaxy galaxy = new Galaxy();
+            Console.ReadLine();
         }
     }
     public class Galaxy 
     {
-        public long NumStars = 100; // trecientas mil millones de estrellas como META
-        public string Name = "Galaxia Zero";
+        public long NumStars = 100; // 300,000,000,000 - 500,000,000,000 stars in our Milky Way galaxy
+        public string Name = "Simulated Galaxy";
 
-        void Generate() 
+        public Galaxy() 
+        {
+            GenerateStars();
+        }
+
+        private void GenerateStars() 
         {
             // Generate planets and cosmis stuff
             for (long i = 1; i <= NumStars; i++)
             {
-                Planet newplanet = new Planet();
+                Star newStar = new Star();
+                newStar.Index = i;
+                newStar.Name = "star-" + Guid.NewGuid().ToString();
+                System.Console.WriteLine($"\nNew STAR generated: {newStar.Index}-{newStar.Name}");
             }
         }
     }    
-    public class Planet 
+    public class Star
     {
-        public int Index { get; set; }
+        private int MAX_PLANETS = 6; 
+        public long Index {get; set;}
         public string Name {get; set;}
+        public string Type { get; set; }
+        public int Age {get; set;}
+        public int Mass {get; set; }
+        public int Density {get; set;}
+        public int Temperature {get; set;}
+        public bool CanHoldLife {get; set;}
 
-        private void Generate() 
+        Random random = new Random();
+        public Star() 
         {
-
+            GeneratePlanets();
+        }
+        private void GeneratePlanets() 
+        {
+            for (int i = 1; i <= random.Next(0, MAX_PLANETS); i++) 
+            {
+                Planet newPlanet = new Planet();
+                newPlanet.Index = i;
+                newPlanet.Name = "planet-" + Guid.NewGuid().ToString();
+                System.Console.WriteLine($"\nNew PLANET generated: {newPlanet.Index}-{newPlanet.Name}");
+            }
         }
     }
+    public class Planet 
+    {
+        public long Index { get; set; }
+        public string Name {get; set;}
+        public bool CanHoldLife {get; set;}
+        
+        private int Mass {get; set;}
+        private int Density {get; set;}
+        private int Atmosphere {get; set;}
+
+        private void GenerateLife() 
+        {
+            // Generate Life if suitable conditions are available
+        }
+    }
+    
 }
 
