13ba1bb07bba0d93f6804b68f730863e870861c7
Diego <d_diazmendoza@hotmail.com>
2020-11-17 13:04:10 -0600

    add planets

--- Program.cs
+++ Program.cs
@@ -2,12 +2,36 @@
 
 namespace AlienLifeSimulator
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
-    
+    public class Galaxy 
+    {
+        public long NumStars = 100; // trecientas mil millones de estrellas como META
+        public string Name = "Galaxia Zero";
+
+        void Generate() 
+        {
+            // Generate planets and cosmis stuff
+            for (long i = 1; i <= NumStars; i++)
+            {
+                Planet newplanet = new Planet();
+            }
+        }
+    }    
+    public class Planet 
+    {
+        public int Index { get; set; }
+        public string Name {get; set;}
+
+        private void Generate() 
+        {
+
+        }
+    }
 }
+
